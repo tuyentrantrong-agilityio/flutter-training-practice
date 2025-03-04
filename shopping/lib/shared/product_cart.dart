e183d06418915f36ff32dfbfcc0ddd5832c3b7b8
Tran Trong Tuyen <trantrongtuyen@Tuyens-Mac-mini.local>
2025-03-04 17:35:12 +0700

    create common widget product card, product quantity section

--- shopping/lib/shared/product_cart.dart
+++ shopping/lib/shared/product_cart.dart
@@ -1,69 +1,86 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:shopping/shared/product_quantity_section.dart';
 import 'package:shopping/utils/extensions/build_context_extension.dart';
 import 'package:shopping/utils/extensions/double_extension.dart';
 
 import '../../../../theme/theme.dart';
+import '../router/app_router.gr.dart';
 
 class ProductCard extends StatelessWidget {
   final String name;
-  final String image;
-  final String type;
-  final double cost;
+  final String imageUrl;
+
+  final double price;
+  final int inventoryQuantity;
 
   const ProductCard({
     super.key,
     required this.name,
-    required this.image,
-    required this.type,
-    required this.cost,
+    required this.imageUrl,
+    required this.price,
+    required this.inventoryQuantity,
   });
 
   @override
   Widget build(BuildContext context) {
     final TextTheme textTheme = context.textTheme;
+    final isCartPage = context.router.current.name != GuardRoute.name;
 
     return SizedBox(
-      width: 157,
-      child: Column(
+      height: 120,
+      child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          SizedBox(
-            width: 157,
-            height: 200,
-            child: DecoratedBox(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                image: DecorationImage(
-                  image: AssetImage(image),
-                  fit: BoxFit.contain,
-                ),
+          Container(
+            width: 120,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              image: DecorationImage(
+                image: AssetImage(imageUrl),
+                fit: BoxFit.cover,
               ),
-              child: Align(
-                alignment: Alignment.bottomRight,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: SvgPicture.asset(
-                    'assets/images/add_cart.svg',
-                    width: 30,
-                    height: 30,
+            ),
+          ),
+          const SizedBox(width: 15),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  name,
+                  style:
+                      textTheme.labelSmall?.copyWith(color: AppColors.gray600),
+                ),
+                const SizedBox(height: 4),
+                Text(
+                  price.toCurrencyFormat,
+                  style: textTheme.labelSmall?.copyWith(
+                    fontWeight: AppFontWeights.bold,
+                    color: AppColors.gray900,
                   ),
                 ),
+                const Spacer(),
+                if (isCartPage)
+                  ProductQuantitySection(inventoryQuantity: inventoryQuantity),
+              ],
+            ),
+          ),
+          Column(
+            children: [
+              const Icon(
+                Icons.cancel_outlined,
+                size: 24,
               ),
-            ),
-          ),
-          const SizedBox(height: 10),
-          Text(
-            name,
-            style: textTheme.labelSmall?.copyWith(color: AppColors.gray600),
-          ),
-          const SizedBox(height: 4),
-          Text(
-            cost.toCurrencyFormat,
-            style: textTheme.labelSmall?.copyWith(
-              fontWeight: AppFontWeights.bold,
-              color: AppColors.gray900,
-            ),
+              const Spacer(),
+              if (!isCartPage)
+                SvgPicture.asset(
+                  'assets/images/add_cart.svg',
+                  width: 25,
+                  height: 25,
+                ),
+            ],
           ),
         ],
       ),
---
+++ shopping/lib/shared/product_quantity_section.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+class ProductQuantitySection extends HookWidget {
+  final int inventoryQuantity;
+  const ProductQuantitySection({
+    super.key,
+    required this.inventoryQuantity,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final _quantity = useState(1);
+
+    return Row(
+      children: [
+        QuantityButton(
+          icon: Icons.add,
+          onPressed: () =>
+              _quantity.value >= inventoryQuantity ? null : _quantity.value++,
+        ),
+        const SizedBox(width: 10),
+        Text(
+          '${_quantity.value}',
+          style: const TextStyle(
+            color: Color(0xFF232323),
+            fontSize: 18,
+            fontFamily: 'Nunito Sans',
+            fontWeight: FontWeight.w600,
+            letterSpacing: 0.90,
+          ),
+        ),
+        const SizedBox(width: 10),
+        QuantityButton(
+          icon: Icons.remove,
+          onPressed: () => _quantity.value <= 1 ? null : _quantity.value--,
+        ),
+      ],
+    );
+  }
+}
+
+class QuantityButton extends StatelessWidget {
+  final IconData icon;
+  final VoidCallback onPressed;
+
+  const QuantityButton({
+    super.key,
+    required this.icon,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Opacity(
+      opacity: 0.40,
+      child: Container(
+        width: 30,
+        height: 30,
+        decoration: BoxDecoration(
+          color: const Color(0xFFE0E0E0),
+          borderRadius: BorderRadius.circular(6),
+        ),
+        child: IconButton(
+          icon: Icon(icon, color: const Color(0xFF232323)),
+          iconSize: 18,
+          padding: EdgeInsets.zero,
+          onPressed: onPressed,
+        ),
+      ),
+    );
+  }
+}
