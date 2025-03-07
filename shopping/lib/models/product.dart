fe0b96cce5d098fef25c80a49c2b4a4d7d19caad
Tran Trong Tuyen <trantrongtuyen@Tuyens-Mac-mini.local>
2025-03-07 17:44:44 +0700

    apply freezed to model of project

---
+++ shopping/lib/models/product.dart
@@ -0,0 +1,22 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'product.g.dart';
+part 'product.freezed.dart';
+
+@freezed
+class Product with _$Product {
+  const factory Product({
+    @JsonKey(name: 'product_id') int? productId,
+    required String name,
+    String? description,
+    required double price,
+    required int stock,
+    @JsonKey(name: 'category_id') required int categoryId,
+    @JsonKey(name: 'image_url') String? imageUrl,
+    @JsonKey(name: 'created_at') required DateTime createdAt,
+    @JsonKey(name: 'updated_at') required DateTime updatedAt,
+  }) = _Product;
+
+  factory Product.fromJson(Map<String, dynamic> json) =>
+      _$ProductFromJson(json);
+}
---
+++ shopping/lib/models/user.dart
@@ -0,0 +1,17 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'user.g.dart';
+part 'user.freezed.dart';
+
+@freezed
+class User with _$User {
+  const factory User({
+    @JsonKey(name: 'user_id') int? userId,
+    required String username,
+    required String email,
+    required String password,
+    @JsonKey(name: 'created_at') required DateTime createdAt,
+  }) = _User;
+
+  factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
+}
