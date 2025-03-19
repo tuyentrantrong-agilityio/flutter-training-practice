722c604167f4864f3e64c63edda34b6480483442
Tuyen Tran <tuyen.trantrong@asnet.com.vn>
2025-03-19 18:09:52 +0700

    update model notification

--- shopping/lib/models/notification.dart
+++ shopping/lib/models/notification.dart
@@ -8,10 +8,10 @@ class Notification with _$Notification {
   const factory Notification({
     @JsonKey(name: 'notification_id') int? notificationId,
     @JsonKey(name: 'user_id') required String userId,
-    required String title,
-    required String message,
-    @JsonKey(name: 'is_read') required bool isRead,
-    @JsonKey(name: 'created_at') required DateTime createdAt,
+    String? title,
+    String? message,
+    @Default(false) @JsonKey(name: 'is_read') bool isRead,
+    @JsonKey(name: 'created_at') DateTime? createdAt,
   }) = _Notification;
 
   factory Notification.fromJson(Map<String, dynamic> json) =>
