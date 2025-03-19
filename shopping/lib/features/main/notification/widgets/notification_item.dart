f8676179cc3c18264af5e397ebb3f03374adb117
Tuyen Tran <tuyen.trantrong@asnet.com.vn>
2025-03-19 18:09:02 +0700

    use function of notification provider in notification page

--- shopping/lib/features/main/notification/widgets/notification_item.dart
+++ shopping/lib/features/main/notification/widgets/notification_item.dart
@@ -1,91 +1,98 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:shopping/const/const.dart';
+import 'package:shopping/utils/extensions/extension.dart';
+
+import '../../../../theme/theme.dart';
 
 class NotificationItem extends HookWidget {
   final String title;
   final String description;
+  final bool isRead;
+  final VoidCallback? onTap;
   const NotificationItem({
     super.key,
     required this.title,
     required this.description,
+    required this.isRead,
+    this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
-    final isRead = useState<bool>(false);
-    return Column(
-      children: [
-        Container(
-          height: 125,
-          color: const Color(0xFFF5F5F5),
-          child: Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(6),
-                  child: Image.asset(
-                    "assets/images/product_image.png",
-                    width: 90,
-                    height: 90,
-                    fit: BoxFit.fill,
-                  ),
+    final TextTheme textTheme = context.textTheme;
+    final isReadState = useState<bool>(isRead);
+
+    return GestureDetector(
+      onTap: () {
+        if (isReadState.value == false) {
+          isReadState.value = true;
+          onTap?.call();
+        }
+      },
+      child: Container(
+        height: 125,
+        color: !isReadState.value ? AppColors.white100 : null,
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(6),
+                child: Image.asset(
+                  "assets/images/onboarding.png",
+                  width: 90,
+                  height: 90,
+                  fit: BoxFit.fill,
                 ),
               ),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 15.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        title,
-                        style: const TextStyle(
-                          color: Color(0xFF232323),
-                          fontSize: 12,
-                          fontFamily: 'Nunito Sans',
-                          fontWeight: FontWeight.w700,
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 15, bottom: 15, right: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      title,
+                      style: textTheme.labelSmall?.copyWith(
+                        fontSize: 12,
+                        fontWeight: AppFontWeights.bold,
+                        color: AppColors.black50,
+                      ),
+                    ),
+                    const SizedBox(height: 6),
+                    Expanded(
+                      child: Text(
+                        description,
+                        textAlign: TextAlign.justify,
+                        style: textTheme.bodyMedium?.copyWith(
+                          fontSize: 11,
+                          fontFamily: AppConst.nunito,
+                          color: AppColors.gray500,
+                          height: 1.5,
                         ),
                       ),
-                      const SizedBox(height: 6),
-                      Expanded(
+                    ),
+                    if (!isReadState.value)
+                      Align(
+                        alignment: Alignment.bottomRight,
                         child: Text(
-                          description,
-                          textAlign: TextAlign.justify,
-                          style: const TextStyle(
-                            color: Color(0xFF808080),
-                            fontSize: 10,
-                            fontFamily: 'Nunito Sans',
-                            fontWeight: FontWeight.w400,
-                            height: 1.50,
+                          context.intl.newNotification,
+                          textAlign: TextAlign.right,
+                          style: textTheme.labelSmall?.copyWith(
+                            color: AppColors.green700,
+                            fontWeight: FontWeight.w900,
                           ),
                         ),
                       ),
-                    ],
-                  ),
+                  ],
                 ),
               ),
-              if (!isRead.value)
-                const Padding(
-                  padding: EdgeInsets.only(right: 16.0, bottom: 8.0),
-                  child: Align(
-                    alignment: Alignment.bottomRight,
-                    child: Text(
-                      'New',
-                      textAlign: TextAlign.right,
-                      style: TextStyle(
-                        color: Color(0xFF27AE60),
-                        fontSize: 14,
-                        fontFamily: 'Nunito Sans',
-                        fontWeight: FontWeight.w800,
-                      ),
-                    ),
-                  ),
-                ),
-            ],
-          ),
+            ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
