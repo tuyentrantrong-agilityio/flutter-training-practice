1ac8f746ad7a463d2031c7bc03bc7e25ccac33cc
Tuyen Tran <tuyen.trantrong@asnet.com.vn>
2025-03-19 18:06:47 +0700

    create notification service and repository

---
+++ shopping/lib/services/notification_service.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+import '../models/notification.dart' as model;
+import 'supabase_init.dart';
+
+class NotificationService {
+  Future<void> insertNotification(
+    String userId,
+    String title,
+    String message,
+  ) async {
+    try {
+      await supabaseClient.from('notifications').insert({
+        'user_id': userId,
+        'title': title,
+        'message': message,
+      });
+    } catch (e) {
+      debugPrint('Exception occurred: $e');
+      rethrow;
+    }
+  }
+
+  Future<List<model.Notification>> getNotifications(String userId) async {
+    try {
+      final response = await supabaseClient
+          .from('notifications')
+          .select()
+          .eq('user_id', userId)
+          .order('created_at', ascending: true);
+
+      return (response as List)
+          .map((json) => model.Notification.fromJson(json))
+          .toList();
+    } catch (e) {
+      debugPrint('Exception occurred: $e');
+      rethrow;
+    }
+  }
+
+  Future<void> updateIsRead(int notificationId) async {
+    try {
+      await supabaseClient
+          .from('notifications')
+          .update({'is_read': true}).eq('notification_id', notificationId);
+    } catch (e) {
+      debugPrint('Exception occurred while updating isRead: $e');
+      rethrow;
+    }
+  }
+}
