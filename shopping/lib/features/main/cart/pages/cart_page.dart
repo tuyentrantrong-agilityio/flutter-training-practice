5522a4bf54d189e2c287ac4cfe9ea7c73d0d03aa
Tuyen Tran <tuyen.trantrong@asnet.com.vn>
2025-03-21 17:15:57 +0700

    set condition for button checkout in cart page

--- shopping/lib/features/main/cart/pages/cart_page.dart
+++ shopping/lib/features/main/cart/pages/cart_page.dart
@@ -1,27 +1,29 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:shopping/utils/extensions/build_context_extension.dart';
 
 import '../../../../l10n/app_localizations.dart';
+import '../../../../models/cart_item_viewmodel.dart';
 import '../../../../providers/cart_provider.dart';
-import '../../../../router/app_router.gr.dart';
 import '../../../../shared/widgets/widget.dart';
 
 @RoutePage()
-class CartPage extends ConsumerWidget {
+class CartPage extends HookConsumerWidget {
   const CartPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final AppLocalizations l10n = context.intl;
     final asyncListProduct = ref.watch(cartNotifierProvider);
+    final cartItemList = useState<List<CartItemViewModel>>([]);
     return MainLayout(
       body: Column(
         children: [
           HeaderWidget(
             leftIcon: Icons.arrow_back_ios,
-            onLeftFunction: () => Navigator.pop(context),
+            onLeftFunction: () => context.router.popForced(),
             title: l10n.myCart,
           ),
           asyncListProduct.when(
@@ -31,6 +33,7 @@ class CartPage extends ConsumerWidget {
                   padding: const EdgeInsets.all(16.0),
                   itemCount: data.length,
                   itemBuilder: (BuildContext context, int index) {
+                    cartItemList.value = data;
                     final cartItemViewModel = data[index];
                     final cartItem = cartItemViewModel.cartItem;
 
@@ -52,7 +55,9 @@ class CartPage extends ConsumerWidget {
             padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
             child: ShoppingButton(
               text: l10n.checkOut,
-              onPressed: () => context.pushRoute(const LogInRoute()),
+              onPressed: () {
+                if (cartItemList.value != []) {}
+              },
               height: 60,
             ),
           ),
