ec22a39fe2aba32951a74489b644c795639a936f
Tuyen Tran <tuyen.trantrong@asnet.com.vn>
2025-05-09 18:21:18 +0700

    config file to handle deeplink

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:app_links/app_links.dart';
 import 'package:cached_query/cached_query.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -7,8 +8,24 @@ import 'package:shopping/firebase_options.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'const/const.dart';
 import 'l10n/app_localizations.dart';
+import 'router/app_router.gr.dart';
 import 'router/app_router_provider.dart';
 import 'theme/theme.dart';
+
+/// StreamProvider để lắng nghe các Deep Links
+final deepLinkProvider = StreamProvider<Uri>((ref) {
+  final appLinks = AppLinks();
+
+  // Lắng nghe các liên kết sâu qua uriLinkStream
+  final stream = appLinks.uriLinkStream;
+
+  // Khi provider bị hủy, hủy luôn stream subscription
+  ref.onDispose(() {
+    stream.listen(null).cancel();
+  });
+
+  return stream;
+});
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -37,6 +54,17 @@ class MyApp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final appRouter = ref.watch(appRouterProvider);
+    // Listen to deep links
+    ref.listen<AsyncValue<Uri>>(deepLinkProvider, (previous, next) {
+      next.whenData((uri) {
+        if (uri.path == 'shopping.dynalinks.app/products/5') {
+          appRouter.push(const CartRoute());
+        } else if (uri.path == '/cart') {
+          appRouter.push(const CartRoute());
+        }
+        // Add more paths if needed
+      });
+    });
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       title: AppConst.appName,
