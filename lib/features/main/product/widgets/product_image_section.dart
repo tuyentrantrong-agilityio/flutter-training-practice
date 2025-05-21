54533109d40eb2e18a5f41916f01ac38432d84d7
Tuyen Tran <tuyen.trantrong@asnet.com.vn>
2025-05-21 15:31:27 +0700

    update back button of ProductImageSection widget

--- lib/features/main/product/widgets/product_image_section.dart
+++ lib/features/main/product/widgets/product_image_section.dart
@@ -4,6 +4,7 @@ import '../../../../theme/theme.dart';
 
 class ProductImageSection extends StatelessWidget {
   final String imageUrl;
+
   const ProductImageSection({
     super.key,
     required this.imageUrl,
@@ -13,51 +14,62 @@ class ProductImageSection extends StatelessWidget {
   Widget build(BuildContext context) {
     return Expanded(
       flex: 3,
-      child: Container(
-        margin: const EdgeInsets.only(left: 52),
-        decoration: BoxDecoration(
-          color: AppColors.lightGrey,
-          borderRadius: const BorderRadius.only(
-            bottomLeft: Radius.circular(50),
-          ),
-          image: DecorationImage(
-            image: NetworkImage(imageUrl),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Stack(
-          children: [
-            Positioned(
-              top: 20,
-              left: -10,
-              child: Container(
-                height: 40,
-                width: 40,
-                decoration: BoxDecoration(
-                  color: AppColors.white,
-                  borderRadius: BorderRadius.circular(6),
-                  boxShadow: const [
-                    BoxShadow(
-                      color: AppColors.greenBlue50,
-                      blurRadius: 40,
-                      offset: Offset(0, 4),
-                    ),
-                  ],
+      child: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 52),
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(50),
                 ),
-                child: IconButton(
-                  icon: const Icon(
-                    Icons.arrow_back_ios,
-                    color: AppColors.black100,
-                    size: 18,
-                  ),
-                  padding: const EdgeInsets.only(left: 8),
-                  alignment: Alignment.center,
-                  onPressed: () => context.maybePop(),
+                image: DecorationImage(
+                  image: NetworkImage(imageUrl),
+                  fit: BoxFit.cover,
                 ),
               ),
+              child: const SizedBox.expand(),
             ),
-          ],
+          ),
+          Positioned(
+            top: 20,
+            left: 30,
+            child: _BackButton(onPressed: () => context.maybePop()),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _BackButton extends StatelessWidget {
+  final VoidCallback onPressed;
+
+  const _BackButton({required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 40,
+      width: 40,
+      decoration: BoxDecoration(
+        color: AppColors.white,
+        borderRadius: BorderRadius.circular(6),
+        boxShadow: const [
+          BoxShadow(
+            color: AppColors.greenBlue50,
+            blurRadius: 40,
+            offset: Offset(0, 4),
+          ),
+        ],
+      ),
+      child: IconButton(
+        icon: const Icon(
+          Icons.arrow_back_ios_new,
+          size: 18,
+          color: AppColors.black100,
         ),
+        onPressed: onPressed,
       ),
     );
   }
